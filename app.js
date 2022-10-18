f7a050f1b26858f570ba363fd1ba210a90abcee8
HanmaDaiki <daikihanma@outlook.com>
2022-10-18 17:27:14 +0300

    fix-add: handle errors, notfoundpage, routes

--- app.js
+++ app.js
@@ -2,17 +2,43 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const users = require('./routes/users');
+const sign = require('./routes/sign');
+const movies = require('./routes/movies');
+const NotFoundError = require('./errors/NotFoudError');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
   useNewUrlParser: true,
 });
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/', sign);
+app.use('/users', users);
+app.use('/movies', movies);
+
+app.use((req, res, next) => {
+  next(new NotFoundError('Page Not Found'));
+});
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+
+  res
+    .status(statusCode)
+    .send({
+      message: statusCode === 500
+        ? 'Server Error'
+        : message,
+    });
+
+  next();
+});
 
 app.listen(PORT, () => {
-  console.log(`Server start on http:\\localhost:${PORT}`);
+  console.log(`Server start on http://localhost:${PORT}`);
 });
