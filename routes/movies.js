d6fb668d5f35ab7724edce8fc32c1f511ff2bf60
HanmaDaiki <daikihanma@outlook.com>
2022-10-19 20:34:09 +0300

    add: routes movies

--- routes/movies.js
+++ routes/movies.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
+const { getMoviesUser, postMovie, deleteMovie } = require('../controllers/movies');
 
-router.get('/', () => {});
+router.get('/', getMoviesUser);
+router.post('/', postMovie);
+router.delete('/:id', deleteMovie);
 
 module.exports = router;
