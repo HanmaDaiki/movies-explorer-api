4878449ad9343e49aba3ec8917850d2385dbc372
HanmaDaiki <daikihanma@outlook.com>
2022-10-21 18:14:02 +0300

    fix: authorization error

---
+++ middlewares/auth.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const AuthorizationError = require('../errors/AuthorizationError');
+const { JWT_DEV } = require('../utils/config');
+
+const { NODE_ENV, JWT_SECRET } = process.env;
+
+module.exports = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    throw new AuthorizationError();
+  }
+
+  let payload;
+
+  try {
+    payload = jwt.verify(token.replace('Bearer ', ''), NODE_ENV === 'production' ? JWT_SECRET : JWT_DEV);
+  } catch (err) {
+    throw new AuthorizationError();
+  }
+
+  req.user = payload;
+
+  return next();
+};
