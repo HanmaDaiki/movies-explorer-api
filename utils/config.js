44c6629fe26fbb7b28f3b3b86bf57d7829f09ced
HanmaDaiki <daikihanma@outlook.com>
2022-10-23 15:54:11 +0300

    fix: config and prod base adress for db

--- utils/config.js
+++ utils/config.js
@@ -1,4 +1,8 @@
+require('dotenv').config();
+
+const { MONGO_PROD, NODE_ENV } = process.env;
+
 module.exports = {
   JWT_DEV: 'dev-secret',
-  MONGO_ADRESS: 'mongodb://localhost:27017/moviesdb',
+  MONGO_ADRESS: NODE_ENV === 'production' ? MONGO_PROD : 'mongodb://localhost:27017/moviesdb',
 };
