7507797afadcadd3796b99b85cf867868b15b248
HanmaDaiki <daikihanma@outlook.com>
2022-10-23 15:02:47 +0300

    fix: rateLimit create conf file

--- app.js
+++ app.js
@@ -3,23 +3,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
-const rateLimit = require('express-rate-limit');
 const index = require('./routes/index');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const cors = require('./middlewares/cors');
 const { ERROR_500 } = require('./utils/statuscode');
 const { MONGO_ADRESS } = require('./utils/config');
+const apiRequestLimiter = require('./utils/rateLimit');
 
 const { PORT = 3000 } = process.env;
 const app = express();
-
-const apiRequestLimiter = rateLimit({
-  windowMs: 1 * 60 * 1000,
-  max: 10,
-  handler: (req, res) => res.status(429).send({
-    message: 'You sent too many requests. Please wait a while then try again',
-  }),
-});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
