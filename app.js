6fc29f836d66d672de18627fcfb978e5ad24200c
HanmaDaiki <daikihanma@outlook.com>
2022-10-23 16:27:40 +0300

    fix: errorHandler

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const { errors } = require('celebrate');
 const index = require('./routes/index');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const cors = require('./middlewares/cors');
-const { ERROR_500 } = require('./utils/statuscode');
 const { MONGO_DEV } = require('./utils/config');
 const apiRequestLimiter = require('./utils/rateLimit');
+const errorHandler = require('./middlewares/errorHandler');
 
 const { PORT = 3000, NODE_ENV, MONGO_PROD } = process.env;
 const app = express();
@@ -28,19 +28,7 @@ app.use(errorLogger);
 
 app.use(errors());
 
-app.use((err, req, res, next) => {
-  const { statusCode = ERROR_500, message } = err;
-
-  res
-    .status(statusCode)
-    .send({
-      message: statusCode === ERROR_500
-        ? 'Server Error'
-        : message,
-    });
-
-  next();
-});
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server start on http://localhost:${PORT}`);
